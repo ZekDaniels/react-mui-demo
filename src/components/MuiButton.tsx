e84b60d5bc0bdd91d1626cdb83977dd3a2a509fc
Zekeriya Şahin Avşar <zekeriyasahinavsar@Zekeriyas-MacBook-Air.local>
2023-03-09 17:06:45 +0300

    buton details addded

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton, Stack } from "@mui/material";
+import { Button, ButtonGroup, IconButton, Stack } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 export const MuiButton = () => {
@@ -13,28 +13,67 @@ export const MuiButton = () => {
       </Stack>
 
       <Stack spacing={2} direction="row">
-        <Button variant="contained" color="primary">Primary</Button>
-        <Button variant="contained" color="secondary">secondary</Button>
-        <Button variant="contained" color="error">Error</Button>
-        <Button variant="contained" color="warning">Warning</Button>
-        <Button variant="contained" color="info">Info</Button>
-        <Button variant="contained" color="success">Success</Button>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
       </Stack>
 
       <Stack display="block" spacing={2} direction="row">
-        <Button variant="contained" size="small">Small</Button>
-        <Button variant="contained" size="medium">Medium</Button>
-        <Button variant="contained" size="large">Large</Button>
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
       </Stack>
 
       <Stack spacing={2} direction="row">
-        <Button variant="contained" startIcon={<SendIcon/>} onClick={() => alert("Clicked")}>Start Icon</Button>
-        <Button variant="contained" endIcon={<SendIcon/>}>End Icon</Button>
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          onClick={() => alert("Clicked")}
+        >
+          Start Icon
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          End Icon
+        </Button>
         <IconButton aria-label="send">
-            <SendIcon/>
+          <SendIcon />
         </IconButton>
       </Stack>
-      
+
+      <Stack spacing={2} direction="row">
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-aria-label="alignment button group"
+        >
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
     </Stack>
   );
 };
