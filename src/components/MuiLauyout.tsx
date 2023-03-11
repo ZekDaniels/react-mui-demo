62db711793a93c87e2c5e202959c0eb9a24ab427
Zekeriya Şahin Avşar <zekeriyasahinavsar@Zekeriyas-MacBook-Air.local>
2023-03-11 20:41:50 +0300

    stack component

--- src/components/MuiLauyout.tsx
+++ src/components/MuiLauyout.tsx
@@ -1,9 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Divider } from "@mui/material";
 import React from "react";
 
 export const MuiLauyout = () => {
   return (
-    <>
+    <Stack
+      sx={{ border: "1px solid" }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
+    >
+      {/* Box */}
       <Box
         sx={{
           backgroundColor: "primary.main",
@@ -22,6 +28,6 @@ export const MuiLauyout = () => {
         bgcolor="success.light"
         p={2}
       ></Box>
-    </>
+    </Stack>
   );
 };
